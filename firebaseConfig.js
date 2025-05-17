1da1697d61d0f66d7e475763152ff4715a298857
Tarique-Rizwan <111663110+Tarique-Rizwan@users.noreply.github.com>
2025-05-18 03:03:17 +0530

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -3,12 +3,7 @@ import { initializeApp } from "firebase/app"
 import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAirFhY_2_gJvYP8RQi4ReWzQXJVDBczHU",
-  authDomain: "bwstoryapp.firebaseapp.com",
-  projectId: "bwstoryapp",
-  storageBucket: "bwstoryapp.firebasestorage.app",
-  messagingSenderId: "915252175310",
-  appId: "1:915252175310:web:6fb5c03a8ed6b1158d8b9f",
+  // Your Firebase configuration here
 }
 
 const app = initializeApp(firebaseConfig)
